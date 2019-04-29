f0b0069974b4bee26722a80292d9cf8119889703
ainaolu <ainaolu@hotmail.com>
2019-04-29 11:57:48 -0500

    This my first program test

---
+++ AccessmentWeekOne/IsPalindrome1/IsPalindrome1/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace IsPalindrome1
+{
+    class Program
+    {
+        /**two projects, a library project and a unit test project.
+        library should have a class that can tell me whether a string is a palindrome or not.
+        all the behavior of the class should be unit tested.
+        a palindrome reads the same forwards and backwards.
+        for example, these should be considered palindromes:
+            "nurses run"
+            "racecaR"
+            "1221"
+            "never odd, or even."
+        for example, these should not be considered palindromes:
+            "one two one"
+            "123abccba123"**/
+        public static bool IsPalindrome(string Reversiblewords)
+        {
+            int min = 0;
+            int max = Reversiblewords.Length - 1;
+            while (true)
+            {
+                if (min > max)
+                {
+                    return true;
+                }
+                char a = Reversiblewords[min];
+                char b = Reversiblewords[max];
+                if (char.ToLower(a) != char.ToLower(b))
+                {
+                    return false;
+                }
+                min++;
+                max--;
+            }
+        }
+
+ static void Main()
+        {
+            string[] array =
+                {
+                    "civic",
+                    "deleveled",
+                    "Hannah",
+                    "kayak",
+                    "level",
+                    "examiron",
+                    "racecar",
+                    "radar",
+                    "refer",
+                    "reviver",
+                    "easywcf",
+                    "rotator",
+                    "rotor",
+                    "sagas",
+                    "solos",
+                    "stats",
+                    "tenet",
+                    "Csharpstar",
+                    ""
+                };
+
+            foreach (string value in array)
+            {
+                Console.WriteLine("{0} = {1}", value, IsPalindrome(value));
+            }
+
+
+        }
+    }
+}
